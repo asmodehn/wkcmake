1d8f973fc316ca52cd16f56bc07bfd9a811e9c14
AlexV <asmodehn@gmail.com>
2014-02-05 15:46:57 +0900

    improved build script on linux

--- ProjectC/build.sh
+++ ProjectC/build.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 BUILD_DIR="../ProjectC_build"
-SRC_DIR=`pwd`
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILD_TYPE=Release
 ENABLE_TESTS=ON
+UNAME=`uname`
+
+cd $SRC_DIR
 
 set +x
 
 mkdir -p $BUILD_DIR && \
-cd $BUILD_DIR
-
-echo "Running in `pwd` :"
-cmake -DProjectC_BUILD_TYPE=$BUILD_TYPE -DProjectC_ENABLE_TESTS=$ENABLE_TESTS $SRC_DIR
-
-make
-
+cd $BUILD_DIR && \
+echo "Running in $SRC_DIR :" && \
+cmake -DProjectC_BUILD_TYPE=$BUILD_TYPE -DProjectC_ENABLE_TESTS=$ENABLE_TESTS $SRC_DIR && \
+make && \ 
 ctest
 
  
