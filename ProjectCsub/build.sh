7e2f330116a3d1eded824e67a516b7dc9eadd573
AlexV <asmodehn@gmail.com>
2014-02-05 15:17:01 +0900

    avoiding problem where project was built in src_dir if build_dir could not be createdwq

--- ProjectCsub/build.sh
+++ ProjectCsub/build.sh
@@ -8,20 +8,18 @@ UNAME=`uname`
 
 cd $SRC_DIR
 
+if [ -z "${UNAME##*CYGWIN*}" ]; then
+	echo CYGWIN platform detected. Changing path to windows path !;
+	SRC_DIR=`cygpath -w $SRC_DIR`;
+fi
+
 set +x
 
 mkdir -p $BUILD_DIR && \
-cd $BUILD_DIR
-
-if [ -z "${UNAME##*CYGWIN*}" ]; then
-	echo CYGWIN platform detected. Changing path to windows path !
-	SRC_DIR=`cygpath -w $SRC_DIR`
-fi
-echo "Running in $SRC_DIR :"
-cmake -DProjectCsub_BUILD_TYPE=$BUILD_TYPE -DProjectCsub_ENABLE_TESTS=$ENABLE_TESTS $SRC_DIR
-
-make
-
+cd $BUILD_DIR && \
+echo "Running in $SRC_DIR :" && \
+cmake -DProjectCsub_BUILD_TYPE=$BUILD_TYPE -DProjectCsub_ENABLE_TESTS=$ENABLE_TESTS $SRC_DIR && \
+make && \ 
 ctest
 
  
