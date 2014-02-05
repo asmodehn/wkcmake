eef638cf00cea567ad00b1493847bf91995581a8
AlexV <asmodehn@gmail.com>
2014-02-05 15:04:28 +0900

    adding check for cygwin platform and changing source path to windows path, to use windows cmake

--- ProjectCsub/build.sh
+++ ProjectCsub/build.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
 BUILD_DIR="../ProjectCsub_build"
-SRC_DIR=`pwd`
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILD_TYPE=Release
 ENABLE_TESTS=ON
+UNAME=`uname`
+
+cd $SRC_DIR
 
 set +x
 
 mkdir -p $BUILD_DIR && \
 cd $BUILD_DIR
 
-echo "Running in `pwd` :"
+if [ -z "${UNAME##*CYGWIN*}" ]; then
+	echo CYGWIN platform detected. Changing path to windows path !
+	SRC_DIR=`cygpath -w $SRC_DIR`
+fi
+echo "Running in $SRC_DIR :"
 cmake -DProjectCsub_BUILD_TYPE=$BUILD_TYPE -DProjectCsub_ENABLE_TESTS=$ENABLE_TESTS $SRC_DIR
 
 make
