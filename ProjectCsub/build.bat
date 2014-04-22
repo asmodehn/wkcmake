102050067396540f247328e99e3d91f99e43e98a
AlexV <asmodehn@gmail.com>
2014-04-22 15:29:42 +0900

    fixing build.bat for ProjectCsub

--- ProjectCsub/build.bat
+++ ProjectCsub/build.bat
@@ -1,14 +1,41 @@
+@echo off
+setlocal
+
 set BUILD_DIR="build"
-set SRC_DIR="%~dp0"
+set SRC_DIR="%CD%"
 set BUILD_TYPE=Release
 set ENABLE_TESTS=ON
 
-cd %SRC_DIR%
+set ORI_DIR=%CD%
 
-if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+REM this can fail if directory already exist
+mkdir %BUILD_DIR%
+cd %BUILD_DIR%
 
-cd %BUILD_DIR% && ^
-echo "Running in %SRC_DIR% :" && ^
-cmake -DProjectCsub_BUILD_TYPE=%BUILD_TYPE% -DProjectCsub_ENABLE_TESTS=%ENABLE_TESTS% %SRC_DIR% && ^
-echo "TODO : build VS from here" && ^
-echo "TODO:  run tests from here"
+echo "Running in %CD% :"
+cmake -DProjectCsub_BUILD_TYPE=%BUILD_TYPE% -DProjectCsub_ENABLE_TESTS=%ENABLE_TESTS% %SRC_DIR%
+
+@echo off
+
+@if "%VS120COMNTOOLS%"=="" (
+	goto error_no_VS120COMNTOOLSDIR
+	) else (
+	@echo Initializing VS environment
+	call "%VS120COMNTOOLS%\vsvars32.bat"
+	)
+
+@echo Building project
+msbuild ProjectCsub.sln /p:Configuration=%BUILD_TYPE%
+
+ctest
+
+cd %ORI_DIR%
+endlocal
+goto end
+
+
+:error_no_VS120COMNTOOLSDIR
+@echo ERROR: Cannot determine the location of the VS 12.0 Common Tools folder.
+@goto end
+
+:end
