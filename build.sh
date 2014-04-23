0c8dfe47a00a612e4c81f925825c5d728ccfe99f
AlexV <asmodehn@gmail.com>
2014-04-23 13:57:25 +0900

    added projectCsub to build and removed projectB while its broken

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 MAIN_DIR="$( cd "$( dirname "$0}" )" && pwd )"
 
 cd $MAIN_DIR/ProjectC && ./build.sh ; cd $MAIN_DIR
+cd $MAIN_DIR/ProjectCsub && ./build.sh ; cd $MAIN_DIR
 cd $MAIN_DIR/ProjectB && ./build.sh ; cd $MAIN_DIR
