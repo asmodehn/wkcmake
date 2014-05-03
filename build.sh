42769d99a668b50ba2b3152441ed7f7ab49b4def
AlexV <asmodehn@gmail.com>
2014-05-03 15:08:27 +0900

    fixed test data copy on linux. test on windows to be done before master merge

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-MAIN_DIR="$( cd "$( dirname "$0}" )" && pwd )"
+MAIN_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
-cd $MAIN_DIR/ProjectC && ./build.sh ; cd $MAIN_DIR
-cd $MAIN_DIR/ProjectCsub && ./build.sh ; cd $MAIN_DIR
-cd $MAIN_DIR/ProjectB && ./build.sh ; cd $MAIN_DIR
+cd $MAIN_DIR/ProjectA && ./build.sh ; cd $MAIN_DIR
